80fb2833b3a02ed33453feb6aae64c25b170bb6e
PredelinaAsya <54816157+PredelinaAsya@users.noreply.github.com>
2021-10-09 18:10:59 +0300

    fixed id and filtering

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -48,16 +48,20 @@ class _CornerStorageBuilder:
 
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
-    feature_params = dict(maxCorners=None,
-                          qualityLevel=0.05,
-                          minDistance=7,
-                          blockSize=7)
 
     lk_params = dict(winSize=(15, 15),
                      maxLevel=2,
                      criteria=(cv2.TERM_CRITERIA_COUNT | cv2.TERM_CRITERIA_EPS, 10, 0.01))
 
     image_0 = (frame_sequence[0] * 255.0).astype(np.uint8)
+
+    m, n = image_0.shape
+    max_corners_count = m * n // 5000
+
+    feature_params = dict(maxCorners=max_corners_count,
+                          qualityLevel=0.1,
+                          minDistance=15,
+                          blockSize=15)
 
     p0 = cv2.goodFeaturesToTrack(image_0, mask=None, **feature_params).reshape((-1, 2))
 
@@ -67,6 +71,8 @@ def _build_impl(frame_sequence: pims.FramesSequence,
         np.array([feature_params['blockSize']] * len(p0))
     )
     builder.set_corners_at_frame(0, corners)
+
+    max_id = len(p0) - 1
 
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
         image_1 = (image_1 * 255.0).astype(np.uint8)
@@ -78,20 +84,21 @@ def _build_impl(frame_sequence: pims.FramesSequence,
             corners.sizes[st == 1]
         )
 
-        if frame % 5 == 0:
-            new_mask = np.full(image_1.shape, 255).astype(np.uint8)
-            curr_corners = corners.points.round().astype(np.uint8)
-            for i in range(curr_corners.shape[0]):
-                cv2.circle(new_mask, (curr_corners[i, 0], curr_corners[i, 1]), corners.sizes[i, 0], 0, -1)
+        new_mask = np.full(image_1.shape, 255).astype(np.uint8)
+        curr_corners = corners.points.astype(int)
+        for i in range(curr_corners.shape[0]):
+            cv2.circle(new_mask, (curr_corners[i, 0], curr_corners[i, 1]), corners.sizes[i, 0], 0, -1)
 
-            p2 = cv2.goodFeaturesToTrack(image_1, mask=new_mask, **feature_params).reshape((-1,2))
-            last_id = corners.ids.max()
-            new_ids = np.arange(last_id + 1, len(p2) + last_id + 1).reshape((-1,1))
-            new_sz = np.array([feature_params['blockSize']] * len(p2)).reshape((-1,1))
+        p2 = cv2.goodFeaturesToTrack(image_1, mask=new_mask, **feature_params)
+        if p2 is not None:
+            p2 = p2.reshape((-1, 2))
+            new_ids = np.arange(max_id + 1, len(p2) + max_id + 1).reshape((-1, 1))
+            new_sz = np.array([feature_params['blockSize']] * len(p2)).reshape((-1, 1))
+            max_id += (min(max_corners_count, len(corners.ids) + len(new_ids)) - len(corners.ids))
             corners = FrameCorners(
-                np.concatenate((corners.ids, new_ids)),
-                np.concatenate((corners.points, p2)),
-                np.concatenate((corners.sizes, new_sz))
+                np.concatenate((corners.ids, new_ids))[:max_corners_count],
+                np.concatenate((corners.points, p2))[:max_corners_count],
+                np.concatenate((corners.sizes, new_sz))[:max_corners_count]
             )
         builder.set_corners_at_frame(frame, corners)
         image_0 = image_1
